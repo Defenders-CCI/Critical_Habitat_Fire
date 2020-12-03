1440f46227aeb0e9953e62fb866c470ebf211d0f
mjevans26 <evans.michael26@gmail.com>
2020-12-03 16:04:41 -0500

    Add server to app.py

--- app.py
+++ app.py
@@ -69,6 +69,8 @@ box = html.Div(className = 'box',
 
 app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP])
 
+server = app.server
+
 app.layout = dbc.Container([
         dbc.Row([
                 dbc.Col([
